560cdabff60b1ebc7b22071e18ba0d7f6ac43c19
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-26 13:31:44 +0100

    Inizio del progetto

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <MyHeader/>
+    <MyMain/>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+import MyMain from './components/MyMain.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+    MyMain
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
