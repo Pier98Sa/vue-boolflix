4615ba5d418fd1a1d33187b1de8a06effc4bec1e
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-01 13:33:06 +0100

    aggiunta immagine not-found e bandiere

--- src/components/partials/MyCard.vue
+++ src/components/partials/MyCard.vue
@@ -3,34 +3,21 @@
         <ul class="text-white" >
             <!--poster-->
             <img v-if="info.poster_path" class="img-fluid" :src="'https://image.tmdb.org/t/p/w342/'+ (info.poster_path)" alt="">
-            <img v-else src="'https://image.tmdb.org/t/p/w342/movie/940791'" alt="">
+            <img v-else class="img-fluid" src="../../assets/not-found.png" alt="">
+
             <!--nome-->
             <li>{{info.title ? info.title : info.name}}</li>
-            <li>{{info.original_title ? info.original_title: info.original_name }}</li>
+            <li>{{info.original_title ? info.original_title: info.original_name}}</li>
+
             <!--Lingua-->
-            <li v-if="info.original_language == 'en'">
-                <span class="fi fi-gb"></span>
-            </li>
-            <li v-else-if="info.original_language == 'ja'">
-                <span class="fi fi-jp"></span>
-            </li>
-            <li v-else-if="info.original_language == 'zh'">
-                <span class="fi fi-cn"></span>
-            </li>
-            <li v-else-if="info.original_language == 'ko'">
-                <span class="fi fi-kr"></span>
-            </li>
-            <li v-else-if="info.original_language == 'hi'">
-                <span class="fi fi-in"></span>
-            </li>
-            <li v-else>
-                <span  :class="'fi fi-'+ (info.original_language) "></span>
-            </li>
+            <div>
+                <img class="ms_flag" v-if="languages.includes(info.original_language)" :src="require(`../../assets/flags/${info.original_language}.svg`)" alt="">
+                <span v-else>{{info.original_language}}</span>
+            </div>
+
             <!--voto-->
             <div>
-
                 <i v-for="i in 5" :key="i" class="fa-star" :class="(i < getStar) ? 'fa-solid' : 'fa-regular'"></i>
-
             </div>
             
              
@@ -44,6 +31,11 @@ export default {
     props:{
         'info': Object
     },
+    data(){
+        return{
+            languages: ['en', 'es','fr','hi','it','ja','ko','pt','zh']
+        }
+    },
     computed:{
         getStar() {
             return Math.ceil(this.info.vote_average / 2);
@@ -54,6 +46,8 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-    
+    .ms_flag{
+        width: 20px;
+    }
 
 </style>
