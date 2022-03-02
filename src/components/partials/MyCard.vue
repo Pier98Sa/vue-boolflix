63ad3668b3cacb31fefe2b2f10db122d52e444e1
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-02 19:11:22 +0100

    aggiornata la grafica di MyCard

--- src/components/partials/MyCard.vue
+++ src/components/partials/MyCard.vue
@@ -1,27 +1,30 @@
 <template>
-    <div class="flip-box my-3">
+    <div class="flip-box my-3 ">
         <div class="flip-box-inner">
 
             <div class="flip-box-front">
+                <!--Poster-->
                 <img v-if="info.poster_path" class="poster-img" :src="'https://image.tmdb.org/t/p/w342/'+ (info.poster_path)" alt="">
                 <img v-else  class="poster-img" src="../../assets/movie.jpg" alt="">
             </div>
 
             <div class="flip-box-back p-1">
-                <div>{{info.title ? info.title : info.name}}</div>
-                <div>{{info.original_title ? info.original_title: info.original_name}}</div>
+                <!--Titolo-->
+                <div> <span class="fw-bolder">Titolo: </span>  {{info.title ? info.title : info.name}}</div>
+                <div><span class="fw-bolder">Titolo originale: </span> {{info.original_title ? info.original_title: info.original_name}}</div>
 
-            
+                <!--Lingua-->
                 <div>
                     <img class="ms_flag" v-if="languages.includes(info.original_language)" :src="require(`../../assets/flags/${info.original_language}.svg`)" alt="">
                     <span v-else>{{info.original_language}}</span>
                 </div>
 
-            
-                <div> <i v-for="i in 5" :key="i" class="fa-star" :class="(i < getStar) ? 'fa-solid' : 'fa-regular'"></i></div>
-            
+                <!--Valutazione-->
+                <div> <span class="fw-bolder">Voto: </span>   <i v-for="i in 5" :key="i" class="fa-star" :class="(i < getStar) ? 'fa-solid' : 'fa-regular'"></i></div>
+
+                <!--Riassunto-->
                 <div class="resume">
-                    {{info.overview}}
+                   <span class="fw-bolder">Overview: </span>  {{info.overview}}
                 </div>
             </div>
         </div>
@@ -55,8 +58,8 @@ export default {
 @import '../../style/variables.scss';
     .flip-box {
         background-color: transparent;
-        height: 342px;
-        width: 208px;
+        height: 100%;
+        width: 100%;
         perspective: 1000px;
 
         
@@ -65,7 +68,7 @@ export default {
             position: relative;
             width: 100%;
             height: 100%;
-            transition: transform 0.8s;
+            transition: transform 1s;
             transform-style: preserve-3d;
 
             .flip-box-front, .flip-box-back {
