c06b054077dc6c099182a312c6fef02b62581723
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-02 19:23:49 +0100

    aggiustata la row in caso di dimensione inferiore ai 600px

--- src/components/FilmsList.vue
+++ src/components/FilmsList.vue
@@ -1,6 +1,6 @@
 <template> 
     <div class="row row-cols-xl-5 row-cols-lg-4 row-cols-md-3 row-cols-sm-2  ">
-        <MyCard v-for="(Film, index) in Films" :key="index" :info="Film" class="col  ms_col-height"/>
+        <MyCard v-for="(Film, index) in Films" :key="index" :info="Film" class="col-2 g-4 ms_col-height"/>
     </div>
       
 </template>
@@ -21,9 +21,8 @@ export default {
 
 <style lang="scss" scoped>
     .ms_col-height{
-        width: 208px;
+        width: 218px;
         height: 342px;
-        margin: 0 5px;
     }
 
 
--- src/components/SerieTvList.vue
+++ src/components/SerieTvList.vue
@@ -1,6 +1,6 @@
 <template> 
     <div class="row row-cols-xl-5 row-cols-lg-4 row-cols-md-3 row-cols-sm-2 ">
-        <MyCard v-for="(Serie, index) in Series" :key="index" :info="Serie" class="col g-4 ms_col-height"/> 
+        <MyCard v-for="(Serie, index) in Series" :key="index" :info="Serie" class="col-2 g-4 ms_col-height"/> 
     </div>
      
 </template>
@@ -21,8 +21,7 @@ export default {
 
 <style lang="scss" scoped>
     .ms_col-height{
-        width: 208px;
+        width: 218px;
         height: 342px;
-        margin: 0 5px;
     }
 </style>
